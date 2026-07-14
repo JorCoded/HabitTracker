0765127cadd3359a141673a901f286d9692235ac
JorCoded <jgkathan@student.udm.ac.mu>
2026-07-14 19:28:54 +0400

    Added styling with tailwind and added placeholder

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -4,7 +4,7 @@ import Button from "./Button"
 function HabitForm ()  {
   return (
     <form className="flex gap-2">
-        <input type="text" name="" className="flex-1 rounded-lg bg-zinc-800" />
+        <input type="text" name="" placeholder="New habit...." className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500" />
         <Button>Add Habit</Button>
     </form>
   )
