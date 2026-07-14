94d2dacd971225b222dd754558a83ea1a82890bb
JorCoded <jgkathan@student.udm.ac.mu>
2026-07-14 17:13:39 +0400

    Made a Header component with basic styling.

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,22 @@ export default function App() {
 
   return (
     <div>
+      <Header/>
     </div>
   )
 }
 
+function Header(){
+  return(
+    <header className="flex items-center justify-between">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-3xl font-bold">Habit Tracker</h1>
+        <span className="">1 / 1 done today</span>
+      </div>
+      <div className="">
+        Div 2
+      </div>
+    </header>
+  )
+}
 
