e8f21705d8e0f7308ecb122c3321b46cbb29d0cc
JorCoded <jgkathan@student.udm.ac.mu>
2026-07-15 22:47:13 +0400

    Added a simlpe form handling in the HabitForm component

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,13 +1,26 @@
 //import React from 'react'
 import Button from "./Button"
+import { useState, type SubmitEvent } from "react";
+
 
 function HabitForm ()  {
+    const [name, setName] = useState("");
+
+    function handleSubmit(e:SubmitEvent){
+        e.preventDefault()
+
+        if(name.trim()==="")return;
+        setName("");
+    }
+
   return (
-    <form className="flex gap-2">
-        <input type="text" name="" placeholder="New habit...." className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500" />
-        <Button className="rounded-lg px-4 py-2 font-medium">Add Habit</Button>
+    <form className="flex gap-2" onSubmit={handleSubmit}>
+        <input value={name} onChange={e=>setName(e.target.value)} type="text" name="" placeholder="New habit...." className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500" />
+        <Button disabled={name.trim()===""} className="rounded-lg px-4 py-2 font-medium">Add Habit</Button>
     </form>
   )
 }
+
+
 
 export default HabitForm
